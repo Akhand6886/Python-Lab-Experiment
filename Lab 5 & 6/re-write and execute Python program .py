83e3fca8baf78d1e87b8737272db061acf5ae1fb
Akhand Pratap Singh <72747530+Akhand6886@users.noreply.github.com>
2021-10-25 23:36:58 +0530

    Update re-write and execute Python program .py

--- Lab 5 & 6/re-write and execute Python program .py
+++ Lab 5 & 6/re-write and execute Python program .py
@@ -9,3 +9,18 @@ def sum_of_number(n):
         x=x+i
     return x
 sum_of_number(n)        
+
+
+#   2. Print Fibonacci series till nth term (Take input from user).
+
+
+n=int(input("Enter the number:"))
+def fib(n):
+    if n==1:
+        return 0
+    elif n==2:
+        return 1
+    else:
+        return fib(n-1)+fib(n-2)
+for i in range(1, n+1):
+    print(fib(i), end=" ")
