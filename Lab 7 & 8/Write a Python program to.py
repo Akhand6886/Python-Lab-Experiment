6eca58d37ece4f0b76583c8ec3137f2ddd692978
Akhand Pratap Singh <72747530+Akhand6886@users.noreply.github.com>
2021-10-26 11:39:33 +0530

    Update Write a Python program to.py

--- Lab 7 & 8/Write a Python program to.py
+++ Lab 7 & 8/Write a Python program to.py
@@ -4,6 +4,7 @@
 #   2. add backslash (\) before every double quote in the ﬁle contents.
 #   3. write it to another ﬁle in the same folder.
 #   4. print the contents of both the ﬁles
+
 #   For example:
 #   If the ﬁrst ﬁle is 'TestFile1.txt' with text as:
 #   Jack said, "Hello Pune".
