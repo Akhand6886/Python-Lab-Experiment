f919ff734e9a762aad8cd8262ba789e065c78470
Akhand Pratap Singh <72747530+Akhand6886@users.noreply.github.com>
2021-10-26 11:41:23 +0530

    Update Print the error code in the case of ZeroDivisionError or ValueError.py

--- Lab 9/Print the error code in the case of ZeroDivisionError or ValueError.py
+++ Lab 9/Print the error code in the case of ZeroDivisionError or ValueError.py
@@ -1,9 +1,12 @@
 #   Q1. Input two values from user where the first line contains N, the number of test cases.
 #   The next N lines contain the space separated values of a and b. Perform integer division and print a/b. Print the error code in the case of ZeroDivisionError or ValueError.
+
+
 #   Sample input
 #   1 0
 #   2 $
 #   3 1
+
 #   Sample Output
 #   Error Code: integer division or modulo by zero
 #   Error Code: invalid literal for int() with base 10: '$'
