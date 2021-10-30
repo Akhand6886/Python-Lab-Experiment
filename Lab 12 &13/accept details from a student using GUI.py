780903a04bc81455f6f186a9a8b6f0a2c0b0d800
Akhand Pratap Singh <72747530+Akhand6886@users.noreply.github.com>
2021-10-30 09:39:53 +0530

    Create accept details from a student using GUI.py

---
+++ Lab 12 &13/accept details from a student using GUI.py
@@ -0,0 +1,10 @@
+#          Q3. Write a program to accept following details from a student using GUI 
+#          1. Name of the student (using Textbox) 
+#          2. Gender (Using radio button) 
+#          3. Qualification (Using List) 
+#          4. Marks of three subjects (using Textbox) 
+#          Compute the percentage of the student and display it in a textbox. 
+
+
+from tkinter import *
+def submitt()
